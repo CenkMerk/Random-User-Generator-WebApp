d3141e8455f331f69fbb761ae41cb7689518abf2
Cenk Merk <cenkmerk@gmail.com>
2023-08-14 01:59:09 +0300

    Added loading page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
+import "./App.css";
 import axios from "axios";
+import LoadingPage from "./components/LoadingPage/LoadingPage";
 
 function App() {
   const [userData, setUserData] = useState();
@@ -8,6 +10,8 @@ function App() {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
+        // Wait for 2 seconds before making the GET request for LoadingPage
+        await new Promise((resolve) => setTimeout(resolve, 2000));
         // Make a GET request to the Random User API
         const response = await axios.get("https://randomuser.me/api");
         // Update the 'userData' state with the fetched user data
@@ -21,7 +25,7 @@ function App() {
     fetchUsers(); // Call the fetchUserData function when the component mounts
   }, []);
 
-  return <div>App</div>;
+  return <div className="App">{userData ? <>loaded</> : <LoadingPage />}</div>;
 }
 
 export default App;
---
+++ src/components/LoadingPage/LoadingPage.tsx
@@ -0,0 +1,7 @@
+import PacmanLoader  from "react-spinners/PacmanLoader";
+
+const LoadingPage = () => {
+  return <PacmanLoader  color="#36d7b7" />;
+};
+
+export default LoadingPage;
