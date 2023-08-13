fd4ccb3bfc876203f90843f71f81d8766dd335d3
Cenk Merk <cenkmerk@gmail.com>
2023-08-14 00:34:09 +0300

    Added random user data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,26 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  const [userData, setUserData] = useState();
+
+  // Function to fetch user data from the Random User API
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        // Make a GET request to the Random User API
+        const response = await axios.get("https://randomuser.me/api");
+        // Update the 'userData' state with the fetched user data
+        setUserData(response.data.results[0]);
+      } catch (error) {
+        // Handle errors if the API request fails
+        console.error("An error occurred while fetching data:", error);
+      }
+    };
+
+    fetchUsers(); // Call the fetchUserData function when the component mounts
+  }, []);
+
   return <div>App</div>;
 }
 
